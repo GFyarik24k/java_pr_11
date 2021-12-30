6d37708f2deeeda17aecb8694a4a0df655d1bbdc
Yaroslav Mazhey <yaroslav_1007@mail.ru>
2021-12-31 01:17:04 +0300

    1

--- src/pr_11/AbstractQueue.java
+++
@@ -1,6 +0,0 @@
-package pr_11;
-
-public abstract class AbstractQueue implements Queue {
-    protected int head, tail;
-    protected static final int START_CAPACITY = 16;
-}
--- src/pr_11/Queue.java
+++
@@ -1,9 +0,0 @@
-package pr_11;
-
-public interface Queue {
-    Object dequeue();
-    Object element();
-    void enqueue(Object o);
-    boolean isEmpty();
-    boolean clear();
-}
